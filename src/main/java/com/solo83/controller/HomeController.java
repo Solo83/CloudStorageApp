52a0564d73887f84cae1b9539c80f7c76018f298
Solo83 <gstsamara@gmail.com>
2024-09-10 13:36:24 +0200

    Added remove button for files and subfolders

--- src/main/java/com/solo83/controller/HomeController.java
+++ src/main/java/com/solo83/controller/HomeController.java
@@ -1,10 +1,10 @@
 package com.solo83.controller;
 
 import com.solo83.dto.BreadCrumbDTO;
-import com.solo83.entity.User;
 import com.solo83.service.BreadCrumbService;
 import com.solo83.service.MinioService;
 import com.solo83.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -33,77 +33,71 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String home(@RequestParam(value = "path", required = false) String path, Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth.isAuthenticated()) {
-            Optional<User> user = userService.findByName(auth.getName());
-            if (user.isPresent()) {
-                Long id = user.get().getId();
-                String userName = auth.getName();
-                String userRootFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
-                if (path == null) {
-                    path = "/";
-                }
-                String fullPath = appendUserRootFolder(path, userRootFolder);
-                List<BreadCrumbDTO> breadCrumb = breadCrumbService.getBreadCrumbsChain(fullPath);
-
-                model.addAttribute("userName", userName);
-                model.addAttribute("userObjects", breadCrumb);
-            }
-        }
-        return "index";
+        return Optional.of(SecurityContextHolder.getContext().getAuthentication())
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName)
+                .flatMap(userService::findByName)
+                .map(user -> {
+                    Long id = user.getId();
+                    String userName = user.getName();
+                    String userRootFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
+                    String resolvedPath = Optional.ofNullable(path).orElse("/");
+                    String fullPath = appendUserRootFolder(resolvedPath, userRootFolder);
+                    List<BreadCrumbDTO> breadCrumb = breadCrumbService.getBreadCrumbsChain(fullPath);
+                    model.addAttribute("userName", userName);
+                    model.addAttribute("userObjects", breadCrumb);
+                    return "index";
+                })
+                .orElse("redirect:/"); // Return "index" if user is not found or not authenticated
     }
 
 
     @GetMapping(value = "/home/create")
     @PreAuthorize("hasAuthority('ROLE_USER')")
     public String createUserFolder() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Optional<User> user = userService.findByName(auth.getName());
-        if (user.isPresent()) {
-            Long id = user.get().getId();
-            String userFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
-            minioService.createEmptyFolder(userFolder);
-
-        }
-
-
-    /*  minioService.createEmptyFolder("user-3-files/test1/test1subfolder1");
-        minioService.createEmptyFolder("user-3-files/test2");
-        minioService.createEmptyFolder("user-3-files/test2/test2subfolder1");
-        minioService.createEmptyFolder("user-3-files/test2/test2subfolder1/subfolder1");
-        minioService.createEmptyFolder("user-3-files/test5/test2subfolder1");
-    */
-        return "redirect:/home";
+        return Optional.of(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getName)
+                .flatMap(userService::findByName)
+                .map(user -> {
+                    Long id = user.getId();
+                    String userFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
+                    minioService.createEmptyFolder(userFolder);
+                    return "redirect:/home";
+                })
+                .orElse("redirect:/"); // Default redirect if user is not found
     }
+
 
     @GetMapping(value = "/home/remove")
     @PreAuthorize("hasAuthority('ROLE_USER')")
-    public String removeUserObject() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Optional<User> user = userService.findByName(auth.getName());
-        if (user.isPresent()) {
-            Long id = user.get().getId();
-            String userRootFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
-           // String fullPath = appendUserRootFolder(path, userRootFolder);
-          //  minioService.removeObject(fullPath);
-        }
-
-        return "redirect:/home"; // надо чтобы в ту же папку вернуло где удаляли
+    public String removeUserObject(@RequestParam String pathToObject, HttpServletRequest request) {
+        return Optional.of(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getName)
+                .flatMap(userService::findByName)
+                .map(user -> {
+                    Long id = user.getId();
+                    String userRootFolder = userService.getUserRootFolder(String.valueOf(id.intValue()));
+                    String fullPath = appendUserRootFolder(pathToObject, userRootFolder);
+                    minioService.removeObject(fullPath);
+                    return getPreviousPageByRequest(request).orElse("/home");
+                })
+                .orElse("/"); // Go to home page if user is not found
     }
 
 
     private String appendUserRootFolder(String path, String userRootFolder) {
         String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8);
-
         if (!userRootFolder.endsWith("/")) {
             userRootFolder = userRootFolder + "/";
         }
-
         if (decodedPath.startsWith("/")) {
             decodedPath = decodedPath.substring(1);
         }
-
         return userRootFolder + decodedPath;
+    }
+
+    private Optional<String> getPreviousPageByRequest(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader("Referer")).map(requestUrl -> "redirect:" + requestUrl);
     }
 
 }
